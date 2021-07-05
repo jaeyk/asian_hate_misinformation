0f76a12db6e31ec434166c089580030ff5f1bbcd
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-07-05 13:13:45 -0700

    done word embedding

--- code/04_word_embedding_post.r
+++ code/04_word_embedding_post.r
@@ -1,4 +1,4 @@
-## ---------------------------------------------
+## ----------------------------------------------------
 # Import pkgs
 pacman::p_load(
   data.table,
@@ -29,81 +29,81 @@ file.list <- glue("{here('functions')}/{
 purrr::walk(file.list, source)
 
 
-## ---------------------------------------------
-df <- fread(here("processed_data", "text_ready.csv"))
-
-df <- subset(df, date >= as.Date(c("2020-03-16")))
-
-
-## ---------------------------------------------
-# Remove non word characters 
-df$full_text <- textclean::strip(df$full_text)
-
-# Call stop words dictionary 
-data("stop_words")
-
-# Tidying and filtering 
-tidy_df <- df %>%
-  # Tokenize
-  unnest_tokens("word", full_text) %>%
-  # Remove stop words 
-  anti_join(get_stopwords(), by = "word") 
-  # Add count 
-  add_count(word) %>%
-  # Filter
-  filter(n >= 1000) %>%
-  # Drop the variable 
-  select(-n)
-
-nested_df <- tidy_df %>%
-  nest(words = c(word))
+## ----eval = FALSE------------------------------------
+## df <- fread(here("processed_data", "text_ready.csv"))
+## 
+## df <- df %>%
+##   filter(wuhan_virus == 1 | chinese_virus == 1 |
+##          kung_flu == 1)
+## 
+## df <- subset(df, date >= as.Date(c("2020-03-16")))
 
 
-## ----eval = FALSE-----------------------------
+## ----eval = FALSE------------------------------------
+## # Call stop words dictionary
+## data("stop_words")
+## 
+## # Tidying and filtering
+## tidy_df <- df %>%
+##   # Tokenize
+##   unnest_tokens(bigram, full_text,
+##                 token = "ngrams", n = 2)
+## 
+## filtered_df <- tidy_df %>%
+##   separate(bigram, into = c("first","second"), sep = " ") %>%
+##   anti_join(stop_words, by = c("first" = "word")) %>%
+##   anti_join(stop_words, by = c("second" = "word")) %>%
+##   filter(str_detect(first, "[a-z]") &
+##          str_detect(second, "[a-z]")) %>%
+##   unite(word, first, second)
+## 
+## nested_df <- filtered_df %>%
+##   nest(words = c(word))
+
+
+## ----eval = FALSE------------------------------------
 ## plan(multisession)  ## for parallel processing
 ## 
 ## unnested_df <- nested_df %>%
 ##   mutate(words = future_map(words, slide_windows, 4L)) %>%
 ##   unnest(words)
-## 
+
+
+## ----eval = FALSE------------------------------------
 ## tidy_pmi <- unnested_df %>%
 ##   unite(window_id, date, window_id) %>%
 ##   pairwise_pmi(word, window_id)
 ## 
-## save(unnested_df, tidy_pmi, file = here("processed_data", "tidy_pmi.Rdata"))
+## save(unnested_df, tidy_pmi, file = here("processed_data", "tidy_pmi_post.Rdata"))
 
 
-## ---------------------------------------------
-load(here("processed_data", "tidy_pmi.Rdata"))
+## ----eval = FALSE------------------------------------
+## # 100 dimensions using Singular Value Composition
+## tidy_word_vectors <- tidy_pmi %>%
+##   widely_svd(
+##     item1, item2, pmi,
+##     nv = 100, maxit = 1000
+##   )
+## 
+## saveRDS(tidy_word_vectors, file = here("processed_data", "tidy_word_vectors_post.Rdata"))
 
 
-## ---------------------------------------------
-# 100 dimensions using Singular Value Composition
-tidy_word_vectors <- tidy_pmi %>%
-  widely_svd(
-    item1, item2, pmi,
-    nv = 100, maxit = 1000
-  )
-
-saveRDS(tidy_word_vectors, file = here("processed_data", "tidy_word_vectors_post.Rdata"))
-
-
-## ----eval = FALSE-----------------------------
+## ----eval = FALSE------------------------------------
 ## # Application
 ## 
+## tidy_word_vectors <- readRDS(file = here("processed_data", "tidy_word_vectors_post.Rdata"))
+## 
 ## tidy_word_vectors %>%
-##   nearest_neighbors("visible") %>%
-##   filter(item1 != "visible") %>%
-##   top_n(30, abs(value)) %>%
+##   nearest_neighbors("chinese_virus") %>%
+##   filter(item1 != "chinese_virus") %>%
+##   filter(!str_detect(item1, "http")) %>%
+##   top_n(10, abs(value)) %>%
 ##   mutate(value = round(value,2)) %>%
 ##   rename(word = item1,
 ##          similarity = value)
-## 
-## tidy_word_vectors %>%
-##   nearest_neighbors("chinese virus")
 
 
-## ----eval = FALSE-----------------------------
+## ----eval = FALSE------------------------------------
 ## knitr::purl("04_word_embedding_post.Rmd",
 ##             "04_word_embedding_post.r")
 
