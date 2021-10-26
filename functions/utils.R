12dcc53268acd65f184e730875ff292370c20825
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-10-26 11:29:38 -0700

    update conText function

--- functions/utils.R
+++ functions/utils.R
@@ -1,3 +1,153 @@
+
+conText <- function(formula, data, text_var = 'text', pre_trained, transform = TRUE, transform_matrix, bootstrap = TRUE, num_bootstraps = 20, stratify_by = NULL, permute = TRUE, num_permutations = 100, getcontexts = TRUE, window = 6, valuetype = "fixed", case_insensitive = TRUE, hard_cut = FALSE, verbose = TRUE){
+
+        # extract target word
+        target <- as.character(formula[[2]])
+        # extract covariates
+        covariates <- attr(terms(formula), which = "term.labels")
+        # if getcontexts is specified, get context around target word
+        if(getcontexts){
+            # subset data to where target is present (this step helps speed up the get_context function)
+            if(valuetype == 'fixed') target_present <- grep(target, dplyr::pull(data, text_var), fixed = TRUE)
+            if(valuetype != 'fixed') target_present <- grep(target, dplyr::pull(data, text_var), ignore.case = case_insensitive)
+            data <- data[target_present,]
+            # add document id variable (used for merging back with covariates)
+            data <- data %>% dplyr::mutate(docname = paste0("text", 1:nrow(.)))
+            # apply get_context function (see get_context documentation)
+            context <- get_context(x = dplyr::pull(data, text_var), target = target, window = window, valuetype = valuetype, case_insensitive = case_insensitive, hard_cut = hard_cut, verbose = verbose)
+            # merge with metadata
+            context <- dplyr::left_join(context, data[,c("docname", covariates)], by = "docname") %>% dplyr::mutate('(Intercept)' = 1)
+            # otherwise take the full text as the context to be embedded
+        }else{
+            # add intercept
+            context <- data %>% dplyr::mutate('(Intercept)' = 1) %>% dplyr::mutate(context = dplyr::pull(data, text_var))
+        }
+
+        # embed context to get dependent variable (note, aggregate is set to FALSE as we want an embedding for each instance)
+        embeds_out <- embed_target(context$context, pre_trained = pre_trained, transform_matrix = transform_matrix, transform = transform, aggregate = FALSE, verbose)
+        Y <- embeds_out$target_embedding
+        if(verbose) cat('total observations included in regression:', nrow(Y), '\n')
+
+        # regressors
+        X <- context[embeds_out$obs_included,c('(Intercept)', covariates)]
+        # run full sample ols
+        full_sample_out <- run_ols(Y = Y, X = X)
+        # outputs
+        beta_cofficients <- full_sample_out$betas
+        norm_tibble <- dplyr::tibble(Coefficient = names(full_sample_out$normed_betas), Normed_Estimate = unname(full_sample_out$normed_betas))
+        # -------------------
+        # bootstrapping
+        # -------------------
+        if(bootstrap){
+            if(verbose) cat('starting bootstrapping \n')
+            # bootstrapped ols
+            bootstrap_out <- replicate(num_bootstraps, bootstrap_ols(Y = Y, X = X, stratify_by = stratify_by), simplify = FALSE)
+            # average betas
+            betas <- lapply(bootstrap_out, '[[', 'betas')
+            bs_betas <- Reduce("+",betas)/length(betas)
+            # summary statistics for normed betas
+            normed_betas <- lapply(bootstrap_out, '[[', 'normed_betas') %>% do.call(rbind,.)
+            mean_normed_betas <- apply(normed_betas, 2, mean)
+            stderror_normed_betas <- 1/sqrt(num_bootstraps) * apply(normed_betas, 2, sd)
+            bs_normed_betas <- dplyr::tibble(Coefficient = names(mean_normed_betas), Normed_Estimate = unname(mean_normed_betas), Std.Error = unname(stderror_normed_betas))
+            # output
+            beta_cofficients <- bs_betas
+            norm_tibble <- bs_normed_betas
+            # notice
+            if(verbose) cat('done with bootstrapping \n')
+        }
+        # -------------------
+        # permutation
+        # -------------------
+        if(permute){
+            if(verbose) cat('starting permutations \n')
+            # permuted ols
+            permute_out <- replicate(num_permutations, permute_ols(Y = Y, X = X), simplify = FALSE)
+            # compute empirical p-value
+            permuted_normed_betas <- lapply(permute_out, '[[', 'normed_betas') %>% do.call(rbind,.)
+            empirical_pvalue <- sweep(permuted_normed_betas, 2, 1/full_sample_out$normed_betas, '*')
+            empirical_pvalue <- apply(empirical_pvalue, 2, function(x) sum(x>1)/length(x))
+            # bind with results
+            norm_tibble <- cbind(norm_tibble, Empirical_Pvalue = unname(empirical_pvalue))
+            if(verbose) cat('done with permutations \n')
+        }
+        # -------------------
+        # output
+        # -------------------
+        return(list(betas = beta_cofficients, normed_betas = norm_tibble))
+    }
+    # -----------------------------
+    #
+    # SUB-FUNCTIONS
+    #
+    # -----------------------------
+    #' Permute OLS
+    #'
+    #' Estimate empirical p-value using permutated regression
+    #'
+    #' @param Y vector of regression model's dependent variable (embedded context)
+    #' @param X data.frame of model independent variables (covariates)
+    #'
+    #' @return list with two elements, `betas` = list of beta_cofficients (D dimensional vectors);
+    #' `normed_betas` = tibble with the norm of the non-intercept coefficients
+    #'
+    permute_ols <- function(Y = NULL, X = NULL){
+        # shuffle Y
+        Y_permuted <- Y[sample(1:nrow(Y), replace = FALSE),]
+        # run ols
+        ols_out <- run_ols(Y = Y_permuted, X = X)
+        # output
+        return(ols_out)
+    }
+    #' Bootstrap OLS
+    #'
+    #' Bootstrap model coefficients and standard errors
+    #'
+    #' @param Y vector of regression model's dependent variable (embedded context)
+    #' @param X data.frame of model independent variables (covariates)
+    #' @param stratify_by covariates to stratify when bootstrapping
+    #'
+    #' @return list with two elements, `betas` = list of beta_cofficients (D dimensional vectors);
+    #' `normed_betas` = tibble with the norm of the non-intercept coefficients
+    #'
+    bootstrap_ols <- function(Y = NULL, X = NULL, stratify_by = NULL){
+        # label instances
+        X_bs <- cbind(obs = 1:nrow(X), X)
+        # sample observations with replacement
+        if(is.null(stratify_by)) X_bs <- dplyr::sample_n(X_bs, size = nrow(X_bs), replace = TRUE)else{
+            X_bs <- X_bs %>% dplyr::group_by_at(stratify_by) %>% dplyr::sample_n(size = dplyr::n(), replace = TRUE) %>% dplyr::ungroup()
+        }
+        # subset Y to sampled observations
+        Y_bs <- Y[X_bs$obs,]
+        # remove observation label
+        X_bs <- X_bs[,-1]
+        # run ols
+        ols_out <- run_ols(Y = Y_bs, X = X_bs)
+        # output
+        return(ols_out)
+    }
+    #' Run OLS
+    #'
+    #' Bootstrap model coefficients and standard errors
+    #'
+    #' @param Y vector of regression model's dependent variable (embedded context)
+    #' @param X data.frame of model independent variables (covariates)
+    #'
+    #' @return list with two elements, `betas` = list of beta_cofficients (D dimensional vectors);
+    #' `normed_betas` = tibble with the norm of the non-intercept coefficients
+    #'
+    run_ols <- function(Y = NULL, X = NULL){
+        # convert X to a matrix
+        X_mat <- as.matrix(X, ncol = ncol(X))
+        # compute OLS bets hats
+        betas <- solve(t(X_mat)%*%X_mat)%*%t(X_mat)%*%Y
+        # normed betas
+        vars <- setdiff(colnames(X), "(Intercept)") # identify non-intercept covariates (norm of intercept is not all that informative)
+        normed_betas <- apply(matrix(betas[vars,], nrow = length(vars)), 1, function(x) norm(matrix(x, nrow = 1))) %>% setNames(vars)
+        # output
+        return(list('betas' = betas, 'normed_betas' = normed_betas))
+    }
+
 ## for bootstrapping 95% confidence intervals; Borrowed from Nick Camp's code from Jaren, Nick, and my shared project
 
 theta <- function(x, xdata, na.rm = T) {
@@ -526,11 +676,11 @@ plot_embed <- function(embed) {
 terms2plot <- function(df1, df2, keyword, year) {
 
   bind_rows(df1, df2) %>%
-    group_by(Term) %>%
+    group_by(feature) %>%
     filter(n() > 1) %>%
-    ggplot(aes(x = fct_reorder(Term, Estimate), y = Estimate,
-               ymax = Estimate + 1.96*Std.Error,
-               ymin = Estimate - 1.96*Std.Error, col = label)) +
+    ggplot(aes(x = fct_reorder(feature, value), y = value,
+               ymax = value + 1.96*std.error,
+               ymin = value - 1.96*std.error, col = label)) +
     geom_pointrange() +
     facet_wrap(~trump_speech) +
     coord_flip() +
@@ -548,10 +698,10 @@ terms2plot_sep <- function(df1, df2, keyword, year) {
 
   bind_rows(df1, df2) %>%
     group_by(label) %>%
-    top_n(25, Estimate) %>%
-    ggplot(aes(x = fct_reorder(Term, Estimate), y = Estimate,
-               ymax = Estimate + 1.96*Std.Error,
-               ymin = Estimate - 1.96*Std.Error, col = label)) +
+    top_n(25, value) %>%
+    ggplot(aes(x = fct_reorder(feature, value), y = value,
+               ymax = value + 1.96*std.error,
+               ymin = value - 1.96*std.error, col = label)) +
     geom_pointrange() +
     facet_wrap(~trump_speech) +
     coord_flip() +
